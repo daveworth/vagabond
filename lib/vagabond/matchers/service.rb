72cba7ccfe0a50f2fb380c224a4de5bb44727a7c
Dave Worth <dave@bignerdranch.com>
2013-06-06 08:32:26 -0400

    update service matcher for missing services
    * service(8) will return 0 if a service is missing so simply check its output as well

--- lib/vagabond/matchers/service.rb
+++ lib/vagabond/matchers/service.rb
@@ -4,7 +4,7 @@ module Vagabond
       RSpec::Matchers.define :be_running do
         match do |actual|
           @status = `service #{actual.name} status 2>&1`.chomp
-          $? == 0
+          $? == 0 && @status !~ /^No\b.*?\bpackage installed$/
         end
 
         failure_message_for_should do |actual|
